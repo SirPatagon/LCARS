53d2106fe977823ca99530b6317ecc57005ecf27
SirPatagon <sirpatagon+development@gmail.com>
2023-03-05 23:01:25 +0100

    Auto register Views and ViewModels

--- LCARS/App.axaml.cs
+++ LCARS/App.axaml.cs
@@ -2,48 +2,26 @@ using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using DryIoc;
-using LCARS.ViewModels;
 using LCARS.Views;
-using ReactiveUI;
 using Splat.DryIoc;
-using System.Linq;
 
 namespace LCARS
 {
     public partial class App : Application
     {
-        private IContainer fContainer;
-        public override void Initialize()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+        public override void Initialize() => AvaloniaXamlLoader.Load(this);
 
         public override void OnFrameworkInitializationCompleted()
         {
-            fContainer = new Container().With(r => r.WithConcreteTypeDynamicRegistrations());
+            var container = new Container()
+                .With(c => c.WithAutoConcreteTypeResolution());
 
-            // Maybe Later Implement something like this so 
-            //fContainer.RegisterMany(new[] { typeof(App).Assembly }, (t) =>
-            //{
-            //    if (!t.IsGenericTypeDefinition)
-            //    {
-            //        var interfaces = t.GetInterfaces();
-
-            //        var iView = interfaces.Where(i => i.IsGenericType && typeof(IViewFor<>).Equals(i.GetGenericTypeDefinition())).FirstOrDefault();
-            //        if (iView != null)
-            //            return interfaces;
-            //    }
-            //    return null;
-            //});
-
-            fContainer.Register(typeof(IViewFor<MainWindowViewModel>), typeof(MainWindow), made: Made.Of(FactoryMethod.ConstructorWithResolvableArguments));
-            fContainer.Register(typeof(MainWindowViewModel));
-
-            fContainer.UseDryIocDependencyResolver();
+            container.RegisterViewsAndViewModels();
+            container.UseDryIocDependencyResolver();
 
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-            {            
-                desktop.MainWindow = fContainer.Resolve<MainWindow>();
+            {
+                desktop.MainWindow = container.Resolve<MainWindow>();
             }
 
             base.OnFrameworkInitializationCompleted();
---
+++ LCARS/ContainerConfig.cs
@@ -0,0 +1,33 @@
+﻿using DryIoc;
+using DryIoc.ImTools;
+using ReactiveUI;
+using System.Linq;
+
+namespace LCARS
+{
+    internal static class ContainerConfig
+    {
+        public static void RegisterViewsAndViewModels(this IContainer container)
+        {
+            typeof(App).Assembly.GetTypes().ForEach(type =>
+            {
+                var inter = type.GetInterfaces();
+                if (inter.Any())
+                {
+                    if (inter.Any(i => i == typeof(IReactiveObject)))
+                    {
+                        container.Register(type);
+                        return;
+                    }
+
+                    var iViewFor = inter.FirstOrDefault(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IViewFor<>));
+                    if (iViewFor != null)
+                    {
+                        container.Register(iViewFor, type, made: Made.Of(FactoryMethod.ConstructorWithResolvableArguments));
+                        return;
+                    }
+                }
+            });
+        }
+    }
+}
